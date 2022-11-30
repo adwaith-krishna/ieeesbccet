331e9329bf7e248b701735e71a15949fe9059f3d
sreejith <imsreejithofficial@gmail.com>
2022-12-01 00:49:58 +0530

    added hero initial setup

---
+++ ieeeweb/src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./hero.css"
+
+function Hero() {
+  return (
+    <div className='hero'>
+        <div className='herotxt'> IEEE STUDENT BRANCH</div>
+        </div>
+  )
+}
+
+export default Hero
